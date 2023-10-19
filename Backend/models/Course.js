17f6f367d9b36bb57d839f69f655f4bf273dc9b4
tenamm02 <mtenam.mt@gmail.com>
2023-10-19 15:06:40 -0400

    Delete project
    Restarting project

--- Backend/models/Course.js
+++
@@ -1,9 +0,0 @@
-const courseSchema = new mongoose.Schema({
-    title: String,
-    description: String,
-    content: String,
-    creator: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
- });
- 
- module.exports = mongoose.model('Course', courseSchema);
- 
